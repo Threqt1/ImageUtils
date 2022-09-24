03a37d5f620636faf88b4da7c06f2085a21494ed
Threqt1 <threqt_@outlook.com>
2022-09-24 00:35:12 -0500

    switch packages, upgrade pckges

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,3 @@
-import { WELCOME_MESSAGE } from "./data/text.js";
-import inquirer from "inquirer";
-import prompt from "inquirer-autocomplete-prompt";
-import path from "path";
-import fs from "fs/promises";
-import fuzzy from "fuzzy";
-import type separator from "inquirer/lib/objects/separator.js";
-import dfold from "downloads-folder";
-inquirer.registerPrompt("autocomplete", prompt);
-
 /*
 TODO:
 1. Add a selection for commands after image selected
@@ -15,14 +5,14 @@ TODO:
 3. Add a handler to find commands relevant to image extension, link code with selection
 */
 
-const VALID_FILE_EXTENSIONS = [
-  ".gif",
-  ".png",
-  ".jpg",
-  ".jpeg",
-  ".webp",
-  ".svg",
-];
+import { WELCOME_MESSAGE } from "./data/text.js";
+import inquirer from "inquirer";
+import dfold from "downloads-folder";
+import FileTreeSelectionPrompt from "inquirer-file-tree-selection-prompt";
+import path from "path";
+inquirer.registerPrompt("file-tree-selection", FileTreeSelectionPrompt);
+
+const VALID_FILE_EXTENSIONS = ["gif", "png", "jpg", "jpeg", "webp", "svg"];
 
 async function main() {
   console.log(WELCOME_MESSAGE);
@@ -30,61 +20,20 @@ async function main() {
 }
 
 async function session() {
-  let cache: {
-    [key: string]: string[];
-  } = {};
   inquirer
     .prompt([
       {
-        type: "autocomplete",
+        root: dfold(),
+        enableGoUpperDirectory: true,
+        type: "file-tree-selection",
         name: "filePath",
-        message: "Enter a path to an Image",
-        suggestOnly: true,
-        source: async (_answers: any[], input = dfold()) => {
-          let choices: string[] = [];
-          input = input.replace(/\/+/g, "/");
-          if (input.length == 0) return choices;
-          let parts = path.parse(input);
-          let isDir = false;
-          try {
-            isDir = (await fs.lstat(parts.dir)).isDirectory();
-          } catch (e) {}
-          if (!isDir) return choices;
-          let filePaths = cache[parts.dir];
-          if (filePaths == undefined) {
-            let tempPaths = await fs.readdir(parts.dir);
-            cache[parts.dir] = tempPaths.map((r) => {
-              let extension = r.split(".").length > 1;
-              if (extension) {
-                return parts.dir + "/" + r;
-              } else {
-                return parts.dir + "/" + r + "/";
-              }
-            });
-            filePaths = cache[parts.dir];
-          }
-
-          let validChoices: (string | separator)[] = fuzzy
-            .filter(parts.base.trim(), filePaths!)
-            .map((r) => r.original);
-          validChoices.push(new inquirer.Separator());
-          return validChoices;
-        },
-        validate: async (input) => {
-          let isValidFile = false;
-          try {
-            await fs.access(input, fs.constants.R_OK);
-            isValidFile = true;
-          } catch (e) {}
-          if (!isValidFile) return "Please input a valid file!";
-          let inputSplit: string[] = input.split(".");
-          let ext = inputSplit[inputSplit.length - 1];
-          if (!VALID_FILE_EXTENSIONS.includes("." + ext!.toLowerCase()))
-            return (
-              "Please input a supported image file! Supported extensions are: " +
-              VALID_FILE_EXTENSIONS.join(", ")
-            );
-          return true;
+        message: "Enter a path to an image",
+        validate: (input: string) => {
+          let split = input.split(path.sep).at(-1);
+          if (split === undefined) return false;
+          split = split.split(".").at(-1);
+          if (split === undefined) return false;
+          return VALID_FILE_EXTENSIONS.includes(split.toLowerCase());
         },
       },
     ])
